0e07249815ea09f370676cf4009301880974b0ae
dag <dag.heyman@gmail.com>
2015-07-01 21:06:03 +0200

    Placeholder file for unit tests

---
+++ unittests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Here goes the unit tests...
+
+import unittest
+
+class tests(unittest.TestCase):
+
+    def testOne(self):
+        self.assertTrue(True);
+
+if __name__ == '__main__':
+    unittest.main()
