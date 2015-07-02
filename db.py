09e679091b59e416b7453046d2cd7f578f4a3058
dag <dag.heyman@gmail.com>
2015-07-02 23:59:20 +0200

    DatabaseOperator is now a class

--- db.py
+++ db.py
@@ -1,41 +1,48 @@
 #!/usr/bin/env python
 
-# This module is responsible for talking to the PSQL database. All queries go here.
 
 import psycopg2
 import configparser
 
 # Load configs
-config = configparser.ConfigParser()
-config.read("./conf.ini")
-db_name = config.get("ConfigSection", "db_name")
-db_user = config.get("ConfigSection", "db_user")
+#config = configparser.ConfigParser()
+#config.read("./conf.ini")
+#db_name = config.get("ConfigSection", "db_name")
+#db_user = config.get("ConfigSection", "db_user")
+
+class DatabaseOperator:
+
+    """This class is responsible for talking to the PSQL database. All queries go here."""
+
+    def __init__(self, db_name, db_user):
+        self.db_name = db_name
+        self.db_user = db_user
+
+    # Get all the users in the db
+    def get_all_users(self):
+        conn = psycopg2.connect("dbname=" + self.db_name + " user=" + self.db_user)
+        cur = conn.cursor()
+        cur.execute("select * from users;")
+        users = cur.fetchall()
+        cur.close()
+        conn.commit()
+        conn.close()
+        return users;
 
 
-# Get all the users in the db
-def get_all_users():
-    conn = psycopg2.connect("dbname=" + db_name + " user=" + db_user)
-    cur = conn.cursor()
-    cur.execute("select * from users;")
-    users = cur.fetchall()
-    cur.close()
-    conn.commit()
-    conn.close()
-    return users;
-
-
-# Get all the tasks in the db
-def get_all_tasks():
-    conn = psycopg2.connect("dbname=" + db_name + " user=" + db_user)
-    cur = conn.cursor()
-    cur.execute("select * from tasks;")
-    tasks = cur.fetchall()
-    cur.close()
-    conn.commit()
-    conn.close()
-    return tasks;
+    # Get all the tasks in the db
+    def get_all_tasks(self):
+        conn = psycopg2.connect("dbname=" + self.db_name + " user=" + self.db_user)
+        cur = conn.cursor()
+        cur.execute("select * from tasks;")
+        tasks = cur.fetchall()
+        cur.close()
+        conn.commit()
+        conn.close()
+        return tasks;
 
 
 if __name__ == "__main__":
-        print(get_all_users())
-        print(get_all_tasks())
+        db = DatabaseOperator(db_name, db_user)
+        print(db.get_all_users())
+        print(db.get_all_tasks())
