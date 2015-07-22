16f0da7d608d0c94713f9271647252375b1c6fbe
dag <dag.heyman@gmail.com>
2015-07-22 16:14:14 +0200

    Listen on the user buttons.

--- static/script.js
+++ static/script.js
@@ -5,6 +5,7 @@ httpRequest.onreadystatechange = function() {
             && httpRequest.status === 200) {
         var json = JSON.parse(httpRequest.responseText);
         createBtns(json.users);
+        listenOnBtns();
     }
 }
 httpRequest.open('GET', 'http://localhost:8080/api/users', true);
@@ -19,7 +20,17 @@ function createBtns(users) {
     for (var i = 0; i < users.length; i++) {
         var button = document.createElement('button');
         button.id = users[i].id;
+        button.className = 'user-button';
         button.innerHTML = users[i].name;
         cleaning.appendChild(button);
+    }
+}
+
+function listenOnBtns() {
+    var buttons = document.getElementsByClassName('user-button');
+    for (var i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function(e) {
+            console.log(e.currentTarget.innerHTML + " is clicked!");
+        });
     }
 }
