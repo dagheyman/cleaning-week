1a9ca0c24c9355b3f0845793a49834c2291850c1
dag <dag.heyman@gmail.com>
2015-07-22 16:45:10 +0200

    Add wrapping self executing function

--- static/script.js
+++ static/script.js
@@ -1,36 +1,52 @@
+(function() {
 
-var httpRequest = new XMLHttpRequest();
-httpRequest.onreadystatechange = function() {
-    if (httpRequest.readyState === 4 
-            && httpRequest.status === 200) {
-        var json = JSON.parse(httpRequest.responseText);
-        createBtns(json.users);
-        listenOnBtns();
+    /**
+     * Get the users from the API
+     */
+    function getUsers() {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === 4 
+                    && httpRequest.status === 200) {
+                var json = JSON.parse(httpRequest.responseText);
+                createBtns(json.users);
+                listenOnBtns();
+            }
+        }
+
+        httpRequest.open('GET', 'http://localhost:8080/api/users', true);
+        httpRequest.send(null);
     }
-}
-httpRequest.open('GET', 'http://localhost:8080/api/users', true);
-httpRequest.send(null);
 
-/**
- * Create a button for every user.
- */
-function createBtns(users) {
-    var cleaning = document.getElementById('cleaning');
+
+   /**
+    * Create a button for every user.
+    */
+    function createBtns(users) {
+        var cleaning = document.getElementById('cleaning');
   
-    for (var i = 0; i < users.length; i++) {
-        var button = document.createElement('button');
-        button.id = users[i].id;
-        button.className = 'user-button';
-        button.innerHTML = users[i].name;
-        cleaning.appendChild(button);
+        for (var i = 0; i < users.length; i++) {
+            var button = document.createElement('button');
+            button.id = users[i].id;
+            button.className = 'user-button';
+            button.innerHTML = users[i].name;
+            cleaning.appendChild(button);
+        }
     }
-}
 
-function listenOnBtns() {
-    var buttons = document.getElementsByClassName('user-button');
-    for (var i=0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", function(e) {
-            console.log(e.currentTarget.innerHTML + " is clicked!");
-        });
+
+   /**
+    * Listen on click events on all user buttons
+    */
+    function listenOnBtns() {
+        var buttons = document.getElementsByClassName('user-button');
+        for (var i=0; i < buttons.length; i++) {
+            buttons[i].addEventListener("click", function(e) {
+                console.log(e.currentTarget.innerHTML + " is clicked!");
+            });
+        }
     }
-}
+
+    getUsers();
+
+})();
