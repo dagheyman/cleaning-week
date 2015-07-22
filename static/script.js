05f8d3e6d3c72cbe7c7660bbb799a42676e01dd3
dag <dag.heyman@gmail.com>
2015-07-22 15:15:57 +0200

    Add buttons for every user

--- static/script.js
+++ static/script.js
@@ -1,15 +1,21 @@
-console.log("Running js..");
 
 var httpRequest = new XMLHttpRequest();
 httpRequest.onreadystatechange = function() {
     if (httpRequest.readyState === 4 
             && httpRequest.status === 200) {
         var json = JSON.parse(httpRequest.responseText);
-
-        console.log(json);
+        createBtns(json.users);
     }
 }
 httpRequest.open('GET', 'http://localhost:8080/api/users', true);
 httpRequest.send(null);
 
-
+/**
+ * Create a button for every user.
+ */
+function createBtns(users) {
+    var cleaning = document.getElementById('cleaning');
+    for (var i = 0; i < users.length; i++) {
+        cleaning.innerHTML += '<button id=' + users[i].id +  '>' + users[i].name + '</button>';
+    }
+}
