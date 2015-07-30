189217a2fc3526c4e2721117b5069603b0f2f10f
dag <dag.heyman@gmail.com>
2015-07-30 15:10:53 +0200

    Add a back button

--- static/script.js
+++ static/script.js
@@ -54,7 +54,6 @@
             if (httpRequest.readyState === 4
                     && httpRequest.status === 200) {
                 var json = JSON.parse(httpRequest.responseText);
-                removeChildren('cleaning');
                 showTask(json.task);
                 console.log(json.task);
             }
@@ -66,25 +65,37 @@
 
 
     /**
-     * Show a specific task
+     * The view for showing a specific task
      */
     function showTask(task) {
+        
+        removeChildren('cleaning');
         var cleaning = document.getElementById('cleaning');
         
-        var title = document.createElement('div');
-        var description = document.createElement('div');
-        var status = document.createElement('div');
-
-        title.className = 'task-field';
-        description.className = 'task-field';
-        status.className = 'task-field';
-
+        // Create a back button to go back to the main menu.
+        var back = document.createElement('a');
+        back.href = window.location;
+        back.innerHTML = '<<<';
+        back.className = 'back-btn';
+        cleaning.appendChild(back);
+        
+        // Element for the task title 
+        var title = document.createElement('div'); 
         title.innerHTML = "Titel: " + task.title;
-        description.innerHTML = "Beskrivning: " + task.description;
-        status.innerHTML = "Klar: " + task.status;
-
+        title.className = 'task-field'; 
         cleaning.appendChild(title);
+
+        // Element for the task description
+        var description = document.createElement('div'); 
+        description.innerHTML = "Beskrivning: " + task.description;
+        description.className = 'task-field';
         cleaning.appendChild(description);
+        
+        // Element for the task status
+        var statusString = task.status ? "Klar" : "Ej klar";
+        var status = document.createElement('div');
+        status.className = 'task-field';
+        status.innerHTML = "Klar: " + statusString;
         cleaning.appendChild(status);
     }
 
